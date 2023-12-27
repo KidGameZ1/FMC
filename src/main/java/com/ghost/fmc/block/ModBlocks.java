ea8284de571cc07a2eea24b072ab0a81ccf2f72a
KidGameZ1 <101521961+KidGameZ1@users.noreply.github.com>
2023-12-27 10:09:26 -0600

    Added Recipes and Loot tables

--- src/main/java/com/ghost/fmc/block/ModBlocks.java
+++ src/main/java/com/ghost/fmc/block/ModBlocks.java
@@ -2,11 +2,16 @@ package com.ghost.fmc.block;
 
 import com.ghost.fmc.FMCMod;
 import com.ghost.fmc.items.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,13 +24,13 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, FMCMod.MOD_ID);
 
     public static final  RegistryObject<Block> BLOCK_OF_STORMITE = registryBlock("stormite_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).strength(5.0F, 6.0F)));
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
 
     public static final  RegistryObject<Block> DEEPSLATE_STORMITE_ORE = registryBlock("deepslate_stormite_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_DIAMOND_ORE)));
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_DIAMOND_ORE), UniformInt.of(3,6)));
 
     public static final  RegistryObject<Block> STORMITE_ORE = registryBlock("stormite_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE)));
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE), UniformInt.of(2,5)));
 
     private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
